50ce2e6551e363f0b4af425fa6382030332ed1ec
EC2 Default User <ec2-user@ip-172-31-70-121.ec2.internal>
2020-12-09 16:24:49 +0000

    first commit

---
+++ CPP_Project/application/app.py
@@ -0,0 +1,47 @@
+import os
+
+from flask import Flask, render_template, request, redirect, send_file, url_for
+
+from s3 import list_files, download_file, upload_file
+
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+BUCKET = "rb-test-myapp-bucket"
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def entry_point():
+    return render_template('main.html')
+    #return 'Hello World!'
+
+
+@app.route("/storage")
+def storage():
+   contents = list_files(BUCKET)
+   return render_template('storage.html', contents=contents)
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    if request.method == "POST":
+        f = request.files['file']
+        f.save(f.filename)
+        upload_file(f"{f.filename}", BUCKET)
+
+        return redirect("/storage")
+
+
+@app.route("/download/<filename>", methods=['GET'])
+def download(filename):
+    if request.method == 'GET':
+        output = download_file(filename, BUCKET)
+
+        return send_file(output, as_attachment=True)
+
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=8080, debug=True)
